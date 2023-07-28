a4fc56c79e999c75e2dbe656b8572a002d43a4bd
abdelrahman elfiky <elfekyabdo537@gmail.com>
2023-07-28 09:25:46 +0200

    finished OOPTasks

---
+++ OOPTasks/OOPTasks/src/Task1/product.java
@@ -0,0 +1,33 @@
+package Task1;
+
+public class product {
+    private String name;
+    private double price;
+    product ()
+    {
+
+    }
+    product (String name,double price)
+    {
+        this.name=name;
+        this.price=price;
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
